4b17350a9149713d8c623bf50b9d4c190ed4fe64
suzana <antolin.suzana@gmail.com>
2022-10-23 12:10:35 +0200

    Serial port interactive

---
+++ interactive_serial.py
@@ -0,0 +1,32 @@
+import sys
+import getopt
+import os
+import time
+import io
+import serial as serial
+from colorama import Fore
+
+AT_QBAND_GET = "AT+QBAND?"
+AT_GET = "AT"
+
+
+if __name__ == '__main__':
+
+    ser = serial.Serial(port="COM10", baudrate=9600, timeout=60)
+    ser.isOpen()
+    input_command = 1
+    print("Enter commands [E for Exit]")
+    while input_command:
+        input_command = input(">>")
+        if input_command.capitalize() == "E":
+            ser.close()
+            exit()
+        else:
+            ser.write((input_command + '\r').encode('ASCII'))
+            out = ''
+            time.sleep(1)
+            while ser.in_waiting > 0:
+                out += ser.read(1).decode('ASCII')
+            if out != '':
+                print(">> " + out)
+                print(ser.timeout)
--- write_to_file_serial.py
+++ write_to_file_serial.py
@@ -9,26 +9,24 @@ from colorama import Fore
 from colorama import Style
 from collections import deque
 
-# ser = serial.Serial(port="COM10", baudrate=9600,)
-# ser.write(b"AT+QBAND?\r")
-
 if __name__ == '__main__':
+
     with serial.serial_for_url('spy://COM10?file=test.txt', timeout=1) as s:
         print(f"On port : {s.name}")
         s.write(b'AT\r')
-        s.write(b'AT+CGMI\r')
+        s.write(b'AT+CGMI\r')  # Request Manufacturer Identification
         time.sleep(1)
-        s.write(b"AT+QBAND?\r\n")
+        s.write(b"AT+QBAND?\r\n")  # query available bands
         time.sleep(1)
 
-        s.write(b"AT+QSOCON=?\r")
-        #s.write(b"AT+QSOC=1,2,1\r")
+        s.write(b"AT+QSOCON=?\r")  # list of available socket ids, remote address and ports
+        # s.write(b"AT+QSOC=1,2,1\r") #connect socket to remote address and port
+        s.write(b"AT+QENGINFO=?\r")  # query current modem status information
 
         byte_lines = s.readlines()
+
         lines = [x[0:-2] for x in byte_lines]
         # lines_array = [y.split() for y in lines]
-        [print(f" {y} ") for y in lines]
-
-        #[print(f"{Fore.GREEN} {y} ") for y in lines]
+        [print(f"{Fore.GREEN} {y} ") for y in lines]
 
         s.send_break()
